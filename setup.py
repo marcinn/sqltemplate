8b3df0c111fcef08837b5c9fdf4b8cef7df48f90
Marcin Nowak <marcin.j.nowak@gmail.com>
2017-02-10 11:14:30 +0100

    update env classifiers

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='sqltemplate',
-      version='0.5.0',
+      version='0.5.1',
       description='Core library for database querying tools '
                   'based on templates',
       classifiers=[
         "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
@@ -21,7 +17,7 @@ setup(name='sqltemplate',
       author_email='marcin.j.nowak@gmail.com',
       url='https://github.com/marcinn/sqltemplate',
       install_requires=['flatdict>=1.2.0,<2.0.0', 'sqlparse>=0.1.19,<0.2'],
-      keywords='web python django sql template',
+      keywords='python sql template',
       packages=find_packages('.'),
       include_package_data=True,
       test_suite='nose.collector',
